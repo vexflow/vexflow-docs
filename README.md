836097c31a57254f7cacc002da178da612bdfbe0
Ron B. Yeh <ronyeh@gmail.com>
2025-04-22 15:02:32 -0700

    Add an option to generate docs for the current dev revision.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # vexflow-docs
 
-Take a look at [the VexFlow API documentation](https://vexflow.github.io/vexflow-docs)
-
+The [VexFlow documentation](https://vexflow.github.io/vexflow-docs/) will eventually be hosted at https://vexflow.com/docs/.
 
 ## Build
 
-Make sure `vexflow/` and `vexflow-docs/` are sibling folders on your computer.
+Make sure `vexflow/` and `vexflow-docs/` are sibling directories on your computer.
+
+Go into the `vexflow-docs/` directory and run `./make_docs.sh`.
--- make_docs.sh
+++ make_docs.sh
@@ -1,4 +1,5 @@
-VERSION=5.0.0
+# VERSION=5.0.0 # The most recent release of VexFlow.
+VERSION=main # Use this to generate the latest version of the API docs.
 
 echo "Generate API docs for VexFlow" $VERSION
 
@@ -16,14 +17,21 @@ fi
 cd ../vexflow/
 git checkout $VERSION
 SOURCE_DIR=$(pwd)
+
 cd ../vexflow-docs/
 DEST_DIR=$(pwd)
+if [ "$VERSION" = "main" ]; then
+  DEST_DIR="$DEST_DIR/api/dev"
+else
+  DEST_DIR="$DEST_DIR/api/$VERSION"
+fi
 
+echo ""
 echo "Source: " $SOURCE_DIR
-echo "Destination: " $DEST_DIR/api/$VERSION
+echo "Destination: " $DEST_DIR
+echo ""
 
 echo "Press ENTER to continue or CTRL-C to cancel..."
 read -n 1 -s
 
-npx typedoc --out ./temp-docs
-mv temp-docs $DEST_DIR/api/$VERSION
+npx typedoc --out $DEST_DIR
